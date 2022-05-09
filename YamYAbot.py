4c59464a3f0c505cd8cf43a3940e8c660c3275ef
YamYA <tommy9301122tommy@gmail.com>
2022-05-09 19:43:45 +0800

    update_20220509
    重新佈署

--- YamYAbot.py
+++ YamYAbot.py
@@ -156,7 +156,7 @@ def get_ani_image(search_name):
 async def broadcast():
     # wysi
     utc8_time = (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime("%H%M")
-    if utc8_time == '0727' and random.randint(1,10) <= 3: # 時間 且機率發生
+    if utc8_time == '0727' and random.randint(1,14) <= 3: # 時間 且機率發生
         channel = bot.get_channel(842463449467453491) # 指定頻道 (zyoi fan club)
         await channel.send('早安ヽ(○´∀`)ﾉ')
 
